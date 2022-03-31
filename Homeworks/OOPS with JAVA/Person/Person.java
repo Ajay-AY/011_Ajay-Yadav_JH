cb92c21b4a879d4bc7baa8dc27e0af15f510c59d
Ajay-AY <101260953+Ajay-AY@users.noreply.github.com>
2022-03-31 16:02:58 +0530

    Add files via upload

---
+++ Homeworks/OOPS with JAVA/Person/Person.java
@@ -0,0 +1,21 @@
+package Person;
+
+public class Person{
+	String name;
+	
+	
+	Person(String name){
+		this.name = name;
+	}
+	
+	String getName(){
+		return this.name;
+	}
+	
+	
+	void setName(String name){
+		this.name = name;
+	}
+	
+	
+}
