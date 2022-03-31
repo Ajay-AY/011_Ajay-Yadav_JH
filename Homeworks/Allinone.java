8f968e37e33124aad778d09236624c1ddaca990a
Ajay-AY <101260953+Ajay-AY@users.noreply.github.com>
2022-03-31 18:40:35 +0530

    Update Allinone.java

--- Homeworks/Allinone.java
+++ Homeworks/Allinone.java
@@ -374,18 +374,4 @@ class Largest                                          //Largest
 	}
 } */
 
-/*import java.util.Scanner;
-class Allinone                                           //LeapYear
-{
-	public static void main(String [] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt();
-		if(year%400==0||year%4==0&&year%100!=0)
-			System.out.println(year+" Is a Leap Year");
-		else
-			System.out.println(year+" Is not a Leap Year");
-		
-	}
-	
-}*/
+/**/
