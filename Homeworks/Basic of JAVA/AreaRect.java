b6a46b43b5f0d34a624fb5150554da5511629576
Ajay-AY <101260953+Ajay-AY@users.noreply.github.com>
2022-04-01 17:29:17 +0530

    Create AreaRect.java

---
+++ Homeworks/Basic of JAVA/AreaRect.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class AreaRect                               //Area of Rectangle
+{
+	public static void main(String []args)
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter Two numbers : ");
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int Area = a*b;
+	System.out.println("Area of rec : "+Area);
+	
+	}
+}
